74ac7bbd6009f9240aa9c4510f70137e04db1eff
Patricia Fiona <patriciafiona3@gmail.com>
2023-02-16 15:47:17 +0700

    [S04 - Africa] Create Content image slider

---
+++ 04. Africa/SWIFTUI-APP-AFRICA-UPDATED/Students/Africa/Africa/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by Other on 16/02/23.
+//
+
+import Foundation
+
+struct CoverImage: Codable, Identifiable {
+    let id: Int
+    let name: String
+}
