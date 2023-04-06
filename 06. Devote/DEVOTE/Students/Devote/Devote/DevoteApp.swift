c23d31858dcd5a7c67c9c606072cd7227dde8b70
Patricia Fiona <patriciafiona3@gmail.com>
2023-04-06 13:56:17 +0700

    [S06] Init project

---
+++ 06. Devote/DEVOTE/Students/Devote/Devote/DevoteApp.swift
@@ -0,0 +1,20 @@
+//
+//  DevoteApp.swift
+//  Devote
+//
+//  Created by Administrator on 06/04/23.
+//
+
+import SwiftUI
+
+@main
+struct DevoteApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
