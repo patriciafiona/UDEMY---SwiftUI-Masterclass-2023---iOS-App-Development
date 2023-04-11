1704ec764ae06e5bda5a0b0db5e29df28df054ec
Patricia Fiona <patriciafiona3@gmail.com>
2023-04-11 11:12:55 +0700

    [S07] Finished setting up apple watch app

---
+++ 07.Notes/NOTES-APP/Students/Notes/Notes Watch App/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes Watch App
+//
+//  Created by Administrator on 11/04/23.
+//
+
+import SwiftUI
+
+@main
+struct Notes_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
