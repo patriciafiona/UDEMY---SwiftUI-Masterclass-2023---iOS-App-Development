fe85f33541c49e8adbe269f1df4460722560ebdd
Patricia Fiona <patriciafiona3@gmail.com>
2023-04-13 10:23:07 +0700

    [S08] Finished Repeating with ForEach & ScrollView

--- 08. Developers/SWIFTUI-APP-01/Students/Learn by Doing/Learn by Doing/View/ContentView.swift
+++ 08. Developers/SWIFTUI-APP-01/Students/Learn by Doing/Learn by Doing/View/ContentView.swift
@@ -12,10 +12,14 @@ struct ContentView: View {
     
     // MARK: - BODY
     var body: some View {
-        VStack {
-            CardView()
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(alignment: .center, spacing: 20) {
+                ForEach(0..<6) { item in
+                    CardView()
+                }
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
