2144709f9f68b987e01a47b29d87a2fd49298a6b
Patricia Fiona <patriciafiona3@gmail.com>
2023-04-13 09:35:04 +0700

    [S08] Init Project & Assets

---
+++ 08. Developers/SWIFTUI-APP-01/Students/Learn by Doing/Learn by Doing/Learn_by_DoingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_by_DoingApp.swift
+//  Learn by Doing
+//
+//  Created by Administrator on 13/04/23.
+//
+
+import SwiftUI
+
+@main
+struct Learn_by_DoingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 08. Developers/SWIFTUI-APP-01/Students/Learn by Doing/Learn by Doing/View/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Learn by Doing
+//
+//  Created by Administrator on 13/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
