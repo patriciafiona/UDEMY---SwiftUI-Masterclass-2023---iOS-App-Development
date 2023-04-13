205e48c677aeeac4282e31d905099f41c7fccec8
Patricia Fiona <patriciafiona3@gmail.com>
2023-04-13 10:26:55 +0700

    [S08] Finished create data model for the card

---
+++ 08. Developers/SWIFTUI-APP-01/Students/Learn by Doing/Learn by Doing/Resource/Model/CardModel.swift
@@ -0,0 +1,18 @@
+//
+//  CardModel.swift
+//  Learn by Doing
+//
+//  Created by Administrator on 13/04/23.
+//
+
+import SwiftUI
+
+struct Card: Identifiable {
+    var id: UUID = UUID()
+    var title: String
+    var headline: String
+    var imageName: String
+    var callToAction: String
+    var message: String
+    var gradientColor: [Color]
+}
