78d7758b16356a9188f88e4d074361a35f61740d
Patricia Fiona <patriciafiona3@gmail.com>
2023-04-17 11:09:34 +0700

    [S09] Init Project

---
+++ 09. Avocado Recipes/SWIFTUI-APP-02/Students/Avocados/Avocados/App/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Administrator on 17/04/23.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
