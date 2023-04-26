06237780c9a78be923886de658f181a8b84a863e
Patricia Fiona <patriciafiona3@gmail.com>
2023-04-26 17:04:22 +0700

    [S10] Finished add popup gameover
    Need to be fixed: popup background color to white

--- 10. Game/SWIFTUI-APP-04/Students/Slot Machine/Slot Machine/View/Screens/MainView.swift
+++ 10. Game/SWIFTUI-APP-04/Students/Slot Machine/Slot Machine/View/Screens/MainView.swift
@@ -178,6 +178,8 @@ struct MainView: View {
                         self.spinReels()
                         
                         self.checkWinning()
+                        
+                        self.isGameOver()
                     }) {
                         Image("gfx-spin")
                             .renderingMode(.original)
@@ -255,8 +257,72 @@ struct MainView: View {
             )
             .padding()
             .frame(maxWidth: 720)
+            .blur(radius: $showingModal.wrappedValue ? 5 : 0, opaque: false)
             
             // MARK: - POPUP
+            if $showingModal.wrappedValue {
+                ZStack {
+                    Color("ColorTransparentBlack").edgesIgnoringSafeArea(.all)
+                    
+                    //MODAL
+                    VStack(spacing: 0) {
+                        //TITLE
+                        Text("GAME OVER")
+                            .font(.system(.title, design: .rounded))
+                            .fontWeight(.heavy)
+                            .padding()
+                            .frame(minWidth: 0, maxWidth: .infinity)
+                            .background(Color("ColorPink"))
+                            .foregroundColor(.white)
+                        
+                        Spacer()
+                        
+                        //MESSAGE
+                        VStack(alignment: .center, spacing: 16) {
+                            Image("gfx-seven-reel")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(maxHeight: 72)
+                            
+                            Text("Bad luck! You lost all of the coins. \nLet's play again!")
+                                .font(.system(.body, design: .rounded))
+                                .lineLimit(2)
+                                .multilineTextAlignment(.center)
+                                .foregroundColor(.gray)
+                                .layoutPriority(1)
+                            
+                            Button(action: {
+                                self.showingModal = false
+                                self.coins = 100
+                            }) {
+                                Text("New Game".uppercased())
+                                    .font(.system(.body, design: .rounded))
+                                    .fontWeight(.semibold)
+                                    .accentColor(Color("ColorPink"))
+                                    .padding(.horizontal, 12)
+                                    .padding(.vertical, 8)
+                                    .frame(minWidth: 128)
+                                    .background(
+                                        Capsule()
+                                            .strokeBorder(lineWidth: 1.75)
+                                            .foregroundColor(Color("ColorPink"))
+                                    )
+                            }
+                        }
+                        
+                        Spacer()
+                    }
+                }
+                .frame(
+                    minWidth: 280, idealWidth: 280, maxWidth: 320,
+                    minHeight: 260, idealHeight: 280, maxHeight: 320,
+                    alignment: .center
+                )
+                .background(.white)
+                .cornerRadius(20)
+                .shadow(color: Color("ColorTransparentBlack"), radius: 6, x: 0, y: 8)
+            }
+            
         }//: ZSTACK
         .sheet(isPresented: $showingInfoView) {
             InfoView()
